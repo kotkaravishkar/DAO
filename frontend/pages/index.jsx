dcabfb182e8fdab169eae2c4b65417856ce54a19
Avishkar Kotkar <kotkaravi07@gmail.com>
2023-02-22 23:49:36 +0530

    address changed

--- frontend/pages/index.jsx
+++ frontend/pages/index.jsx
@@ -11,19 +11,28 @@ import {
 } from "../constants";
 import styles from "../styles/Home.module.css";
 
-
 export default function Home() {
+  // ETH Balance of the DAO contract
   const [treasuryBalance, setTreasuryBalance] = useState("0");
+  // Number of proposals created in the DAO
   const [numProposals, setNumProposals] = useState("0");
+  // Array of all proposals created in the DAO
   const [proposals, setProposals] = useState([]);
+  // User's balance of CryptoDevs NFTs
   const [nftBalance, setNftBalance] = useState(0);
+  // Fake NFT Token ID to purchase. Used when creating a proposal.
   const [fakeNftTokenId, setFakeNftTokenId] = useState("");
+  // One of "Create Proposal" or "View Proposals"
   const [selectedTab, setSelectedTab] = useState("");
+  // True if waiting for a transaction to be mined, false otherwise.
   const [loading, setLoading] = useState(false);
+  // True if user has connected their wallet, false otherwise
   const [walletConnected, setWalletConnected] = useState(false);
+  // isOwner gets the owner of the contract through the signed address
   const [isOwner, setIsOwner] = useState(false);
   const web3ModalRef = useRef();
 
+  // Helper function to connect wallet
   const connectWallet = async () => {
     try {
       await getProviderOrSigner();
@@ -32,27 +41,34 @@ export default function Home() {
       console.error(error);
     }
   };
-  
 
+  /**
+   * getOwner: gets the contract owner by connected address
+   */
   const getDAOOwner = async () => {
     try {
-        const signer   = await getProviderOrSigner(true);
-        const contract = getDaoContractInstance(signer);
+      const signer = await getProviderOrSigner(true);
+      const contract = getDaoContractInstance(signer);
 
-        const _owner  = await contract.owner();
-        const address = await signer.getAddress();
-        if (address.toLowerCase() === _owner.toLowerCase()) {
-          setIsOwner(true);
-        }
+      // call the owner function from the contract
+      const _owner = await contract.owner();
+      // Get the address associated to signer which is connected to Metamask
+      const address = await signer.getAddress();
+      if (address.toLowerCase() === _owner.toLowerCase()) {
+        setIsOwner(true);
+      }
     } catch (err) {
       console.error(err.message);
     }
   };
 
-  
+  /**
+   * withdrawCoins: withdraws ether by calling
+   * the withdraw function in the contract
+   */
   const withdrawDAOEther = async () => {
     try {
-      const signer   = await getProviderOrSigner(true);
+      const signer = await getProviderOrSigner(true);
       const contract = getDaoContractInstance(signer);
 
       const tx = await contract.withdrawEther();
@@ -66,18 +82,18 @@ export default function Home() {
     }
   };
 
+  // Reads the ETH balance of the DAO contract and sets the `treasuryBalance` state variable
   const getDAOTreasuryBalance = async () => {
     try {
       const provider = await getProviderOrSigner();
-      const balance = await provider.getBalance(
-        CRYPTODEVS_DAO_CONTRACT_ADDRESS
-      );
+      const balance = await provider.getBalance(CRYPTODEVS_DAO_CONTRACT_ADDRESS);
       setTreasuryBalance(balance.toString());
     } catch (error) {
       console.error(error);
     }
   };
 
+  // Reads the number of proposals in the DAO contract and sets the `numProposals` state variable
   const getNumProposalsInDAO = async () => {
     try {
       const provider = await getProviderOrSigner();
@@ -89,6 +105,7 @@ export default function Home() {
     }
   };
 
+  // Reads the balance of the user's CryptoDevs NFTs and sets the `nftBalance` state variable
   const getUserNFTBalance = async () => {
     try {
       const signer = await getProviderOrSigner(true);
@@ -100,6 +117,7 @@ export default function Home() {
     }
   };
 
+  // Calls the `createProposal` function in the contract, using the tokenId from `fakeNftTokenId`
   const createProposal = async () => {
     try {
       const signer = await getProviderOrSigner(true);
@@ -115,7 +133,9 @@ export default function Home() {
     }
   };
 
-  
+  // Helper function to fetch and parse one proposal from the DAO contract
+  // Given the Proposal ID
+  // and converts the returned data into a Javascript object with values we can use
   const fetchProposalById = async (id) => {
     try {
       const provider = await getProviderOrSigner();
@@ -135,7 +155,8 @@ export default function Home() {
     }
   };
 
-  
+  // Runs a loop `numProposals` times to fetch all proposals in the DAO
+  // and sets the `proposals` state variable
   const fetchAllProposals = async () => {
     try {
       const proposals = [];
@@ -150,7 +171,8 @@ export default function Home() {
     }
   };
 
-
+  // Calls the `voteOnProposal` function in the contract, using the passed
+  // proposal ID and Vote
   const voteOnProposal = async (proposalId, _vote) => {
     try {
       const signer = await getProviderOrSigner(true);
@@ -168,7 +190,8 @@ export default function Home() {
     }
   };
 
-  
+  // Calls the `executeProposal` function in the contract, using
+  // the passed proposal ID
   const executeProposal = async (proposalId) => {
     try {
       const signer = await getProviderOrSigner(true);
@@ -185,6 +208,7 @@ export default function Home() {
     }
   };
 
+  // Helper function to fetch a Provider/Signer instance from Metamask
   const getProviderOrSigner = async (needSigner = false) => {
     const provider = await web3ModalRef.current.connect();
     const web3Provider = new providers.Web3Provider(provider);
@@ -202,25 +226,23 @@ export default function Home() {
     return web3Provider;
   };
 
-
+  // Helper function to return a DAO Contract instance
+  // given a Provider/Signer
   const getDaoContractInstance = (providerOrSigner) => {
-    return new Contract(
-      CRYPTODEVS_DAO_CONTRACT_ADDRESS,
-      CRYPTODEVS_DAO_ABI,
-      providerOrSigner
-    );
+    return new Contract(CRYPTODEVS_DAO_CONTRACT_ADDRESS, CRYPTODEVS_DAO_ABI, providerOrSigner);
   };
 
-
+  // Helper function to return a CryptoDevs NFT Contract instance
+  // given a Provider/Signer
   const getCryptodevsNFTContractInstance = (providerOrSigner) => {
-    return new Contract(
-      CRYPTODEVS_NFT_CONTRACT_ADDRESS,
-      CRYPTODEVS_NFT_ABI,
-      providerOrSigner
-    );
+    return new Contract(CRYPTODEVS_NFT_CONTRACT_ADDRESS, CRYPTODEVS_NFT_ABI, providerOrSigner);
   };
 
- 
+  // piece of code that runs everytime the value of `walletConnected` changes
+  // so when a wallet connects or disconnects
+  // Prompts user to connect wallet if not connected
+  // and then calls helper functions to fetch the
+  // DAO Treasury Balance, User NFT Balance, and Number of Proposals in the DAO
   useEffect(() => {
     if (!walletConnected) {
       web3ModalRef.current = new Web3Modal({
@@ -238,13 +260,16 @@ export default function Home() {
     }
   }, [walletConnected]);
 
-
+  // Piece of code that runs everytime the value of `selectedTab` changes
+  // Used to re-fetch all proposals in the DAO when user switches
+  // to the 'View Proposals' tab
   useEffect(() => {
     if (selectedTab === "View Proposals") {
       fetchAllProposals();
     }
   }, [selectedTab]);
 
+  // Render the contents of the appropriate tab based on `selectedTab`
   function renderTabs() {
     if (selectedTab === "Create Proposal") {
       return renderCreateProposalTab();
@@ -254,13 +279,10 @@ export default function Home() {
     return null;
   }
 
+  // Renders the 'Create Proposal' tab content
   function renderCreateProposalTab() {
     if (loading) {
-      return (
-        <div className={styles.description}>
-          Loading... Waiting for transaction...
-        </div>
-      );
+      return <div className={styles.description}>Loading... Waiting for transaction...</div>;
     } else if (nftBalance === 0) {
       return (
         <div className={styles.description}>
@@ -277,7 +299,10 @@ export default function Home() {
             type="number"
             onChange={(e) => setFakeNftTokenId(e.target.value)}
           />
-          <button className={styles.button2} onClick={createProposal}>
+          <button
+            className={styles.button2}
+            onClick={createProposal}
+          >
             Create
           </button>
         </div>
@@ -285,22 +310,20 @@ export default function Home() {
     }
   }
 
+  // Renders the 'View Proposals' tab content
   function renderViewProposalsTab() {
     if (loading) {
-      return (
-        <div className={styles.description}>
-          Loading... Waiting for transaction...
-        </div>
-      );
+      return <div className={styles.description}>Loading... Waiting for transaction...</div>;
     } else if (proposals.length === 0) {
-      return (
-        <div className={styles.description}>No proposals have been created</div>
-      );
+      return <div className={styles.description}>No proposals have been created</div>;
     } else {
       return (
         <div>
           {proposals.map((p, index) => (
-            <div key={index} className={styles.proposalCard}>
+            <div
+              key={index}
+              className={styles.proposalCard}
+            >
               <p>Proposal ID: {p.proposalId}</p>
               <p>Fake NFT to Purchase: {p.nftTokenId}</p>
               <p>Deadline: {p.deadline.toLocaleString()}</p>
@@ -328,8 +351,7 @@ export default function Home() {
                     className={styles.button2}
                     onClick={() => executeProposal(p.proposalId)}
                   >
-                    Execute Proposal{" "}
-                    {p.yayVotes > p.nayVotes ? "(YAY)" : "(NAY)"}
+                    Execute Proposal {p.yayVotes > p.nayVotes ? "(YAY)" : "(NAY)"}
                   </button>
                 </div>
               ) : (
@@ -346,8 +368,14 @@ export default function Home() {
     <div>
       <Head>
         <title>CryptoDevs DAO</title>
-        <meta name="description" content="CryptoDevs DAO" />
-        <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="description"
+          content="CryptoDevs DAO"
+        />
+        <link
+          rel="icon"
+          href="/favicon.ico"
+        />
       </Head>
 
       <div className={styles.main}>
@@ -376,25 +404,33 @@ export default function Home() {
             </button>
           </div>
           {renderTabs()}
+          {/* Display additional withdraw button if connected wallet is owner */}
           {isOwner ? (
             <div>
-            {loading ? <button className={styles.button}>Loading...</button>
-                     : <button className={styles.button} onClick={withdrawDAOEther}>
-                         Withdraw DAO ETH
-                       </button>
-            }
+              {loading ? (
+                <button className={styles.button}>Loading...</button>
+              ) : (
+                <button
+                  className={styles.button}
+                  onClick={withdrawDAOEther}
+                >
+                  Withdraw DAO ETH
+                </button>
+              )}
             </div>
-            ) : ("")
-          }
+          ) : (
+            ""
+          )}
         </div>
         <div>
-          <img className={styles.image} src="/cryptodevs/0.svg" />
+          <img
+            className={styles.image}
+            src="/cryptodevs/0.svg"
+          />
         </div>
       </div>
 
-      <footer className={styles.footer}>
-        Made with &#10084; by Crypto Devs
-      </footer>
+      <footer className={styles.footer}>Made with &#10084; by Crypto Devs</footer>
     </div>
   );
 }
